2bb9480f8759f75ffe287e16701ec5a4cc622f06
StarHeartHunt <starheart233@gmail.com>
2023-10-14 15:46:54 +0800

    feat: add external plugins `status` and `apscheduler`

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import nonebot
+from nonebot.adapters.red import Adapter as REDAdapter
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
 nonebot.init()
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
+driver.register_adapter(REDAdapter)
 
-nonebot.load_plugins("src/plugins")
+config = driver.config
+
+nonebot.load_all_plugins(set(config.plugins), {"src/plugins"}.union(config.plugin_dirs))
 
 if __name__ == "__main__":
     nonebot.run()
