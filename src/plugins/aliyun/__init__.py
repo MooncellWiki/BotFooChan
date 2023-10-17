30815b9399ed35695e3b31e170a2bf3d3bb7c471
StarHeartHunt <starheart233@gmail.com>
2023-10-17 16:29:13 +0800

    fix: add parent plugin metadata

--- src/plugins/aliyun/__init__.py
+++ src/plugins/aliyun/__init__.py
@@ -1,8 +1,15 @@
 from pathlib import Path
 
 import nonebot
+from nonebot.plugin import PluginMetadata
 
 from .config import Config
+
+__plugin_meta__ = PluginMetadata(
+    "阿里云",
+    "阿里云开放功能插件",
+    "见子插件使用方法",
+)
 
 # load all aliyun plugin config from global config
 config = Config.parse_obj(nonebot.get_driver().config)
