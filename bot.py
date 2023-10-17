18fc8aebff09546cfb571fc68e9c9da3aef8616c
StarHeartHunt <starheart233@gmail.com>
2023-10-17 20:57:39 +0800

    fix: export app module

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from nonebot.adapters.red import Adapter as REDAdapter
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
 
 nonebot.init()
+app = nonebot.get_asgi()
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
@@ -13,4 +14,4 @@ config = driver.config
 nonebot.load_all_plugins(set(config.plugins), {"src/plugins"}.union(config.plugin_dirs))
 
 if __name__ == "__main__":
-    nonebot.run()
+    nonebot.run(app="__mp_main__:app")
