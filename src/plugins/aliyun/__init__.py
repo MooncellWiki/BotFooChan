fc57508bf7a5e9df943dde0cffaf988e53ff3908
StarHeartHunt <starheart233@gmail.com>
2023-10-17 14:25:48 +0800

    chore: fix typo

--- src/plugins/aliyun/__init__.py
+++ src/plugins/aliyun/__init__.py
@@ -4,9 +4,9 @@ import nonebot
 
 from .config import Config
 
-# load all github plugin config from global config
+# load all aliyun plugin config from global config
 config = Config.parse_obj(nonebot.get_driver().config)
 
-# load all github subplugins
+# load all aliyun subplugins
 _sub_plugins = set()
 _sub_plugins |= nonebot.load_plugins(str((Path(__file__).parent / "plugins").resolve()))
