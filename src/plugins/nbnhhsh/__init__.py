b23e6eaaaf59d997f80924c7559ee1578a40c90e
StarHeartHunt <starheart233@gmail.com>
2023-10-19 10:36:00 +0800

    fix: circular import

--- src/plugins/nbnhhsh/__init__.py
+++ src/plugins/nbnhhsh/__init__.py
@@ -2,7 +2,6 @@ import nonebot
 from nonebot.plugin import PluginMetadata
 
 from .config import Config
-from . import common as common
 
 nbnhhsh_config = Config.parse_obj(nonebot.get_driver().config)
 
@@ -11,3 +10,5 @@ __plugin_meta__ = PluginMetadata(
     "提供拼音首字母缩写查询",
     "/hhsh, /nbnhhsh, /好好说话, /人话 [缩写]：查询拼音首字母缩写",
 )
+
+from . import common as common
