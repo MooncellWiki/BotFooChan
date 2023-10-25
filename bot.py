ad3aed387e0c1323bd0efed27710a57ee1b12a31
StarHeartHunt <starheart233@gmail.com>
2023-10-25 19:38:49 +0800

    chore(deps): add `nonebot-adapter-red`

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import nonebot
 from nonebot.adapters.qq import Adapter as QQAdapter
+from nonebot.adapters.red import Adapter as RedAdapter
 from nonebot.adapters.feishu import Adapter as FeishuAdapter
 from nonebot.adapters.satori import Adapter as SatoriAdapter
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
@@ -11,8 +12,9 @@ driver = nonebot.get_driver()
 config = driver.config
 
 driver.register_adapter(QQAdapter)
-driver.register_adapter(FeishuAdapter)
+driver.register_adapter(RedAdapter)
 driver.register_adapter(SatoriAdapter)
+driver.register_adapter(FeishuAdapter)
 driver.register_adapter(ONEBOT_V11Adapter)
 
 nonebot.load_all_plugins(set(config.plugins), {"src/plugins"}.union(config.plugin_dirs))
