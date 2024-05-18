37bc01484c0774215a7458da1b8aa8e2afaf719e
StarHeartHunt <starheart233@gmail.com>
2024-05-18 17:58:15 +0800

    fix: bili parse regex

--- src/plugins/bili_parser/consts.py
+++ src/plugins/bili_parser/consts.py
@@ -1,6 +1,6 @@
 BILI_DATA = "_bili_data"
 
-AV_REGEX = r".*av(\d).*"
-BV_REGEX = r".*BV([A-Za-z0-9]*).*"
+AV_REGEX = r".*(av\d).*"
+BV_REGEX = r".*(BV[A-Za-z0-9]*).*"
 
 SHARE_CLICK_ENDPOINT = "https://api.biliapi.net/x/share/click"
